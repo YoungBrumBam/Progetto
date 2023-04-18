113f6434b4c61c9081055fcf179a065ae36cfa96
YoungBrumBam <Tms02c01@>
2023-04-18 11:57:03 +0200

    ccdsacda

--- src/Moduli/Agenda.java
+++ src/Moduli/Agenda.java
@@ -5,12 +5,11 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Agenda {
-    private ArrayList<Appuntamento> appuntamenti;
     public String nome;
 
     public Agenda(String nome) {
         this.nome = nome;
-        appuntamenti=new ArrayList<>();
+        ArrayList<Appuntamento> appuntamenti = new ArrayList<>();
     }
 
     public Appuntamento appuntamentoAggiungi(LocalDate data, LocalTime orario, int durata, String nome, String luogo){
